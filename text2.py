f2ce4b11655532296b7a2f46c84f68d111de593c
Thonia1226 <feyithonia@gmail.com>
2024-09-06 10:51:56 -0400

    I just added fresh files

---
+++ text2.py
@@ -0,0 +1,12 @@
+#Add three numbers together and subtract one number from the result
+x=50
+y=50
+p=300
+q=x+y+p-180
+print (q) 
+print ("I am there")
+if q<100:
+    print("Yes q is less than 100")
+else:
+    print ("No q is greater than 100")
+    
---
+++ userinput.py
@@ -0,0 +1,6 @@
+#A python code that will take input from the user
+print ("Enter your firstname")
+firstname = input ()
+print ("Enter your surname")
+surname = input ()
+print (firstname + " " + surname)
