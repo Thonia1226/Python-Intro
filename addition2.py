1b4a0697a01bcf3ff70621a41a5ad3c3f7aa330e
Thonia1226 <feyithonia@gmail.com>
2024-10-17 15:53:28 -0400

    I did python training

---
+++ addition2.py
@@ -0,0 +1,6 @@
+# A function to add three numbers together
+def addition(a,b,c):
+    r=a+b+c
+    print(r)
+
+addition(21,81,91)
---
+++ multiply.py
@@ -0,0 +1,6 @@
+#Write a python function that would multiply three numbers together
+def multiply(a,b,c):
+    d=a*b*c
+    print(d)
+
+multiply(500, 400, 200)
